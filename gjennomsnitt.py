34134114ef09ec59f9514f5263580304eecde75b
domest-9 <121232271+domest-9@users.noreply.github.com>
2025-05-03 16:14:00 +0200

    endret koden til en med avvik

--- gjennomsnitt.py
+++ gjennomsnitt.py
@@ -1,20 +1,35 @@
-import pandas as pd
+sverige_poeng = [88, 100, 72, 107, 170, 30, 170, 51,
+                 47, 33, 185, 372, 62, 218, 365, 261, 344,
+                 274, 334, 109, 438, 583, 174]
+
+norge_poeng = [57, 3, 123, 3, 125, 36, 182,
+               387, 35, 7, 191, 88, 102, 158,
+               144, 331, 75, 182, 268, 16]
 
 def main():
-    land = input("Velg et land du vil finne gjennomsnitt påeng for: ").lower()  # passer på så landet ender opp i lowercase, 
-# ellerss funker ikke programmet siden filene er lagret i lowercase
+    def analyser(poeng, land):
+        sum_poeng = 0
+        antall = len(poeng)
+        maks_poeng = poeng[0]
+        min_poeng = poeng[0]
+
+        # print(f"\nPoeng for {land}:")
+        for p in poeng:
+            # print(p)  # Skjult poengutskrift
+            sum_poeng += p
+            if p > maks_poeng:
+                maks_poeng = p
+            if p < min_poeng:
+                min_poeng = p
+
+        gjennomsnitt = sum_poeng / antall
+        avvik = (maks_poeng - min_poeng) / 2
+
+        print(f"Gjennomsnittlig poengsum for {land} er {gjennomsnitt:.2f} ± {avvik}")
 
 
-# leser Excel filen fra directoriet datasett
-# bruker {} for å sette inn variabelen land i filnavnet
-    datasett = pd.read_excel(f'datasett/{land}.xlsx') 
-    
-    sum_land = datasett['Points'].sum()
-    antall_rader_land = len(datasett)
-    gjennomsnitt_land = sum_land / antall_rader_land
-
-    print(f"Gjennomsnitt påeng for {land.capitalize()}: {gjennomsnitt_land:.2f}")
-# capitalize() for å gjøre første bokstav stor igjen
+    analyser(sverige_poeng, "Sverige")
+    analyser(norge_poeng, "Norge")
 
 if __name__ == "__main__":
     main()
